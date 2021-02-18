4aa051fcf1fbf37a24c9f5213ca2316bfd825b58
Rafael Sabatini <rafael.sabatini@xpi.com.br>
2021-02-17 22:31:29 -0300

    'initial'

---
+++ src/FileUpload.API/Controllers/ProposalController.cs
@@ -0,0 +1,59 @@
+﻿using FileUpload.API.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FileUpload.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProposalController: ControllerBase
+    {
+
+
+
+        [RequestSizeLimit(100000000)]
+        [HttpPost("file-upload")]
+        public async Task<ActionResult<string>> FileUpload (IFormFile file)
+        {
+            var fileName = await SaveFile(file);
+
+            return Ok(fileName);
+        }
+
+        [RequestSizeLimit(100000000)]
+        [HttpPost]
+        public async Task<ActionResult<string>> CreateProposal(ProposalViewModel viewModel)
+        {
+
+            var fileName = await SaveFile(viewModel.FileUpload);
+
+            return Ok(fileName);
+
+        }
+
+        private async Task<string> SaveFile (IFormFile file)
+        {
+            var fileName = $"{Guid.NewGuid()}_{file.FileName}";
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), @$"files\{fileName}");
+
+            using ( var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return fileName;
+
+        }
+
+
+
+        
+    }
+}
---
+++ src/FileUpload.API/ViewModels/ProposalViewModel.cs
@@ -0,0 +1,19 @@
+﻿using FileUpload.API.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUpload.API.ViewModels
+{
+    [ModelBinder(typeof(JsonWithFilesFormDataModelBinder), Name = "proposal")]
+    public class ProposalViewModel
+    {
+
+        public int ProductCode { get; set; }
+
+        public long ProposalCode { get; set; }
+
+        public string CustomerDocument { get; set; }
+
+        public IFormFile FileUpload { get; set; }
+    }
+}
